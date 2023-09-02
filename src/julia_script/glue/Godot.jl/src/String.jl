0f2e6844c68bf79e5b7b47a8938091178b53c9b4
Ricardo Buring <ricardo.buring@gmail.com>
2023-09-02 10:02:04 +0200

    Add proper String type implementation

--- src/julia_script/glue/Godot.jl/src/String.jl
+++ src/julia_script/glue/Godot.jl/src/String.jl
@@ -1,3 +1,10 @@
 mutable struct GodotString
 	cowdata::Ptr{Char}
+	function GodotString(string::String)
+		chars = transcode(UInt16, string * '\0')
+		godot_string = new(C_NULL)
+		@ccall godot_julia_string_new_from_utf16_chars(godot_string::Ref{GodotString}, chars::Ref{UInt16})::Cvoid
+		destroy_string(s) = @ccall godot_julia_string_destroy(s::Ref{GodotString})::Cvoid
+		finalizer(destroy_string, godot_string)
+	end
 end
